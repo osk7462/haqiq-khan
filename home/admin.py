a3d84091c213d58e2d5444dd62ca9968fce20f47
osama khan <osk7462@gmail.com>
2021-02-27 00:35:17 -0800

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import PriceList, OurWork, OurWorkPhoto
+from .models import PriceList, OurWork, OurWorkPhoto, SlideImage
 
 # Register your models here.
 admin.site.register(PriceList)
 admin.site.register(OurWork)
 admin.site.register(OurWorkPhoto)
+admin.site.register(SlideImage)
